d44b1ca85db69bf9d9524abe342bdc50799e5465
Daniel Shokouhi <dshokouhi@gmail.com>
2019-09-07 21:46:10 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyobihai",
+    version="1.0.0",
+    author="Daniel Shokouhi",
+    author_email="dshokouhi@gmail.com",
+    description="A Python wrapper for Obihai",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dshokouhi/pyobihai",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
